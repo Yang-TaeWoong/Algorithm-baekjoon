8d207bdf13e595f952df99523bafbcaef4c881e5
Tae_woong <ytw5754@naver.com>
2018-08-08 15:56:23 +0900

    1991

---
+++ 1991.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+struct node {
+	char left;
+	char right;
+};
+ 
+struct node arr[27];
+ 
+void preorder(char root) {
+    if (root == '.')
+        return;
+    else {
+        cout << root;
+        preorder(arr[root].left);
+        preorder(arr[root].right);
+    }
+}
+ 
+void inorder(char root) {
+    if (root == '.')
+        return;
+    else {
+        inorder(arr[root].left);
+        cout << root;
+        inorder(arr[root].right);
+    }
+}
+ 
+void postorder(char root) {
+    if (root == '.')
+        return;
+    else {
+        postorder(arr[root].left);
+        postorder(arr[root].right);
+        cout << root;
+    }
+}
+ 
+int main() {
+    int n;
+	cin >> n;
+ 
+    char t1, t2, t3;
+ 
+    for (int i = 1; i <= n; i++) {
+        cin >> t1 >> t2 >> t3;
+        arr[t1].left = t2; arr[t1].right = t3;
+    }
+ 
+    preorder('A');
+    cout << "\n";
+    inorder('A');
+    cout << "\n";
+    postorder('A');
+    cout << "\n";
+ 
+    return 0;
+}
+
